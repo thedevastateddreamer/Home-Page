f3dd05088d754ea94363bf7531063034972431a7
Divya Prabha Tyagi <divyaprabhatyagi@Divyas-MacBook-Pro.local>
2020-11-27 21:02:05 +0530

    initial commit

---
+++ lib/main_drawer.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.all(0.0),
+          children: <Widget>[
+            new Container(
+              child: new DrawerHeader(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: ClipOval(
+                    child: Image.network(
+                      "https://lh3.googleusercontent.com/fr7iBUCXzCLWmMa0bU6FOOBGxbL9h6TinmtdByeJmgfEBkQ0LU5eGw7gt0p6CJMNpi92",
+                      width: 130,
+                      height: 130,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              color: Colors.indigo[200],
+            ),
+            ExpansionTile(
+              leading: Icon(Icons.category),
+              title: Text("Categories"),
+              children: <Widget>[
+                ListTile(
+                  title: Text('All'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Hospitals'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Stores'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Banks'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.check_circle_outline),
+              title: Text('Check Status'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.format_align_justify),
+              title: Text('Register Your Business'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Contact Us'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ));
+  }
+}
